2c596a06faceb027b3e9a986157303c5b02fae71
Bomoga <atmorton04@gmail.com>
2026-03-04 20:36:16 -0500

    chore: env setup

--- backend/src/lib/config/settings.py
+++ backend/src/lib/config/settings.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
+    ENVIRONMENT: str = "development"
     PROJECT_NAME: str = "NoteBud API"
     API_V1_STR: str = "/api/v1"
 
@@ -11,11 +13,12 @@ class Settings(BaseSettings):
     POSTGRES_DB: str = "notebud_dev"
 
     GCS_BUCKET_NAME: str = "notebud-dev-bucket"
+    GOOGLE_APPLICATION_CREDENTIALS: str = "./service-account-key.json"
 
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=f".env.{os.getenv('ENVIRONMENT', 'development')}", env_file_encoding="utf-8")
 
 settings = Settings()
