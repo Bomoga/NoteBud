ea674182ebfed019e45f7a56dee0b5c7b73b2197
Bomoga <atmorton04@gmail.com>
2026-03-08 14:33:04 -0400

    feat: initialize alembic and generate first migration

--- backend/src/lib/models/notebook.py
+++ backend/src/lib/models/notebook.py
@@ -7,6 +7,7 @@ class Notebook(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
+    course_code = Column(String(255), nullable=False)
     description = Column(Text, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
