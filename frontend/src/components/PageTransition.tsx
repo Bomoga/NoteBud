7b1eaa64aa8aaf61226af8beb752e1389027c72c
Bomoga <atmorton04@gmail.com>
2026-04-05 20:12:39 -0400

    S5-12: fix white flash by removing mode=wait from AnimatePresence

--- frontend/src/components/PageTransition.tsx
+++ frontend/src/components/PageTransition.tsx
@@ -7,12 +7,12 @@ export default function PageTransition({ children }: { children: React.ReactNode
   const pathname = usePathname();
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence>
       <motion.div
         key={pathname}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        exit={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ duration: 0.25, ease: 'easeOut' }}
         style={{ height: '100%' }}
       >
